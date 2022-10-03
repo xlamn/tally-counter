0988797814020dfa8bedd474748ab2a772c0d3ae
TuanLamNguyen <tuanlam.nguyen@maibornwolff.de>
2022-10-03 11:39:33 +0200

    remove text in overview

--- lib/pages/tally_counters_overview/app_header.dart
+++ lib/pages/tally_counters_overview/app_header.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../constants/constants.dart';
-
 class AppHeader extends StatelessWidget {
   final String title;
   final List<Widget> actions;
@@ -16,24 +14,6 @@ class AppHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverAppBar(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      expandedHeight: 150,
-      toolbarHeight: 70,
-      flexibleSpace: FlexibleSpaceBar(
-        centerTitle: false,
-        titlePadding: const EdgeInsets.only(
-          top: SizeConstants.large,
-          bottom: SizeConstants.normal,
-          right: SizeConstants.xLarge,
-          left: SizeConstants.xLarge,
-        ),
-        title: Text(
-          title,
-          style: TextStyle(
-            fontSize: 24.0,
-            color: Theme.of(context).textTheme.titleMedium!.color,
-          ),
-        ),
-      ),
       actions: actions,
     );
   }
