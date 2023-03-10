a30b11f4c37f712bc5d1a7a4b2853e50735a5770
Lam Nguyen <xnguyen.lam@gmail.com>
2023-03-11 00:05:49 +0100

    update flutter version

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 
 class ColorConstants {
   static final defaultLightColorScheme = ColorScheme.fromSwatch(
-    primarySwatch: Colors.blue,
+    primarySwatch: white,
   );
   static final defaultDarkColorScheme = ColorScheme.fromSwatch(
     primarySwatch: Colors.blue,
     brightness: Brightness.dark,
   );
 }
+
+const MaterialColor white = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Colors.white,
+    100: Colors.white,
+    200: Colors.white,
+    300: Colors.white,
+    400: Colors.white,
+    500: Colors.white,
+    600: Colors.white,
+    700: Colors.white,
+    800: Colors.white,
+    900: Colors.white,
+  },
+);
